7471ad1e1700e80b7c0a1a53611233afbd7104e8
xixiang <xixiang@umich.edu>
2025-01-20 23:14:14 -0500

    Finished Phase 1

---
+++ src/helper.js
@@ -0,0 +1,26 @@
+//Local Storage
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+  return `${existingBudgetLength * 34} 65% 50%`
+}
+
+export const fetchData = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+export const deleteItem = ({ key }) => {
+  return localStorage.removeItem(key);
+};
+
+export const createBudget = ({name, amount}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    color: generateRandomColor()
+  }
+
+  const existingBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem("budgets", JSON.stringify([...existingBudgets, newItem]))
+}
